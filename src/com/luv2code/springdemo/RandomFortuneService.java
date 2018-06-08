d0908a38a1c1f30d8439ee0e03453b4577e761a1
David Stromner <davidstromner@gmail.com>
2018-06-08 13:35:37 +0200

    Annotationss Qualifiers

---
+++ src/com/luv2code/springdemo/RandomFortuneService.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+import java.time.Instant;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService{
+	private String[] fortunes = {"Fortune1", "Fortune2", "Fortune3"};
+	
+	@Override
+	public String getFortune() {
+		return fortunes[Instant.now().getNano()%fortunes.length];
+	}
+
+}
