6fa72a1e9371a1e2adc56202dd9f0b34df9f98f8
David Strömner <david.stromner@alten.se>
2018-06-08 10:51:54 +0200

    Autowiring Constructor

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,13 +1,25 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
-
+	
+	private FortuneService fortuneService;
+	
+	@Autowired // Will look for a class that implements the FortuneService interface
+	public TennisCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "TennisCoach: Play 10 sets";
 	}
-
+	
+	@Override
+	public String getDailyFortune() {
+		return "TennisCoach: " + fortuneService.getFortune();
+	}
 }
